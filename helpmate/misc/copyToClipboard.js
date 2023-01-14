7fd7f9d19abd78584826908c3765ad74c2a06b16
Priyank Parashar <paras20xx@gmail.com>
2023-01-14 16:46:08 +0400

    Updated helpmate/misc/copyToClipboard.js

--- helpmate/misc/copyToClipboard.js
+++ helpmate/misc/copyToClipboard.js
@@ -1,17 +1,23 @@
 /* globals navigator */
 
-// Ref: https://caniuse.com/?search=navigator.clipboard.writeText
-//      * Must be called within user gesture event handlers such as pointerdown or pointerup.
-//      * Writing to the clipboard is available without permission in secure contexts and browser extensions, but only
-//        from user-initiated event callbacks. Browser extensions with the "clipboardWrite" permission can write to the
-//        clipboard at any time.
-const copyToClipboard = async function (simpleText) {
-    if (
+const isCopyToClipboardSupported = function () {
+    const flag = (
         typeof navigator === 'object' &&
         navigator &&
         navigator.clipboard &&
         typeof navigator.clipboard.writeText === 'function'
-    ) {
+    );
+
+    return flag;
+};
+
+const copyToClipboard = async function (simpleText) {
+    if (isCopyToClipboardSupported()) {
+        // Ref: https://caniuse.com/?search=navigator.clipboard.writeText
+        //      * Must be called within user gesture event handlers such as pointerdown or pointerup.
+        //      * Writing to the clipboard is available without permission in secure contexts and browser extensions, but only
+        //        from user-initiated event callbacks. Browser extensions with the "clipboardWrite" permission can write to the
+        //        clipboard at any time.
         await navigator.clipboard.writeText(simpleText);
         return true;
     } else {
@@ -19,4 +25,7 @@ const copyToClipboard = async function (simpleText) {
     }
 };
 
-export { copyToClipboard };
+export {
+    copyToClipboard,
+    isCopyToClipboardSupported
+};
