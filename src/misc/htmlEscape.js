2ef78c256c670e4f843781d6fe9d10ac8ee97c24
priyank <priyank@example.com>
2025-03-26 01:35:48 +0400

    Minor refactoring

--- src/misc/htmlEscape.js
+++ src/misc/htmlEscape.js
@@ -1,12 +1,10 @@
 const htmlEscape = function (str) {
-    /* eslint-disable unicorn/prefer-string-replace-all */
     return str
-        .replace(/&/g, '&amp;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;') // https://stackoverflow.com/questions/2083754/why-shouldnt-apos-be-used-to-escape-single-quotes
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-    /* eslint-enable unicorn/prefer-string-replace-all */
+        .replaceAll('&', '&amp;')
+        .replaceAll('"', '&quot;')
+        .replaceAll("'", '&#39;') // https://stackoverflow.com/questions/2083754/why-shouldnt-apos-be-used-to-escape-single-quotes
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;');
 };
 
 export { htmlEscape };
